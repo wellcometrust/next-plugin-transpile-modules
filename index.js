2a848fbcc3c7f4e082b46df8dc5b721cf96542f5
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2019-01-28 13:41:44 +0100

    Add eslint, remove webpack from devDependencies

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Stolen from https://stackoverflow.com/questions/10776600/testing-for-equality-of-regular-expressions
  */
-function regexEqual(x, y) {
+function regexEqual (x, y) {
   return (x instanceof RegExp) && (y instanceof RegExp) &&
     (x.source === y.source) && (x.global === y.global) &&
     (x.ignoreCase === y.ignoreCase) && (x.multiline === y.multiline);
@@ -11,45 +11,44 @@ function regexEqual(x, y) {
  * Actual Next.js plugin
  */
 module.exports = (nextConfig = {}) => {
-
-  const { transpileModules = [] } = nextConfig
+  const { transpileModules = [] } = nextConfig;
   const includes = transpileModules.map(module => (new RegExp(`${module}(?!.*node_modules)`)));
-  const excludes = [new RegExp(`node_modules(?!\/(${transpileModules.join('|')})(?!.*node_modules))`)];
+  const excludes = [new RegExp(`node_modules(?!/(${transpileModules.join('|')})(?!.*node_modules))`)];
 
   return Object.assign({}, nextConfig, {
-    webpack(config, options) {
+    webpack (config, options) {
       // Safecheck for Next < 5.0
       if (!options.defaultLoaders) {
         throw new Error(
           'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
-        )
+        );
       }
 
       // Avoid Webpack to resolve transpiled modules path to their real path
-      config.resolve.symlinks = false
+      config.resolve.symlinks = false;
       config.externals = config.externals.map(external => {
-        if (typeof external !== 'function') return external
+        if (typeof external !== 'function') return external;
         return (ctx, req, cb) =>
-          (Boolean(includes.find(include => include.test(req))) ? cb() : external(ctx, req, cb))
-      })
+          (includes.find(include => include.test(req)) ? cb() : external(ctx, req, cb));
+      });
 
       // Add a rule to include and parse all modueles
       config.module.rules.push({
         test: /\.+(js|jsx|ts|tsx)$/,
         loader: options.defaultLoaders.babel,
         include: includes
-      })
+      });
 
       if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, options)
+        return nextConfig.webpack(config, options);
       }
 
-      return config
+      return config;
     },
 
     // webpackDevMiddleware needs to be told to watch the changes in the
     // transpiled modules directories
-    webpackDevMiddleware(config) {
+    webpackDevMiddleware (config) {
       // Replace /node_modules/ by the new exclude RegExp (including the modules
       // that are going to be transpiled)
       const ignored = config.watchOptions.ignored.filter(
@@ -57,7 +56,7 @@ module.exports = (nextConfig = {}) => {
       ).concat(excludes);
 
       config.watchOptions.ignored = ignored;
-      return config
+      return config;
     }
-  })
-}
+  });
+};
